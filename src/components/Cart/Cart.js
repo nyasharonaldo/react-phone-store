1e34e91b403f00af62b4e121c6328bac0f5abd47
nyasharonaldo <nyasharonaldo@gmail.com>
2020-01-28 23:12:49 +0000

    second commit

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -12,13 +12,14 @@ export default class Cart extends Component {
                 <ProductConsumer>
                     {value => {
                         const {cart} = value;
+                        
                         if(cart.length>0){
                             return(
                             <React.Fragment>
                                 <Title name='your' title='cart'/>
                                 <CartColumns/>
                                 <CartList value={value}/>
-                                <CartTotals value={value}/>
+                                <CartTotals value={value} history={this.props.history} />
                             </React.Fragment>
                             )
                         }else{
--- src/components/Cart/CartTotals.js
+++ src/components/Cart/CartTotals.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import PayPalButton from './PayPalButton';
 
-export default function CartTotals({value}) {
+export default function CartTotals({value}, history) {
     const {cartSubTotal, cartTax, cartTotal, clearCart} = value;
 
     return (
+        
+        
         <React.Fragment>
+            
             <div className='container'>
+                {console.log(history)}
+                
                 <div className='row'>
                     <div className='col-10 mt-2 ml-sm-5 ml-md-auto
                     col-sm-8 text-capitalize text-right'>
@@ -35,6 +41,11 @@ export default function CartTotals({value}) {
                             </span>
                             <strong> $ {cartTotal} </strong>
                         </h5>
+                        <PayPalButton 
+                            total={cartTotal} 
+                            clearCart={clearCart} 
+                            history={history}
+                        />
                     </div>
                 </div>
             </div>
